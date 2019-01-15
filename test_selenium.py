1d4478fe1ad2c3ada05b4fb2db52f665523c6a42
yanahini <36894930+yanahini@users.noreply.github.com>
2019-01-15 15:59:08 +0100

    Add selenium python file

---
+++ test_selenium.py
@@ -0,0 +1,20 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+username = os.environ["SAUCE_USERNAME"]
+access_key = os.environ["SAUCE_ACCESS_KEY"]
+capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+hub_url = "%s:%s@localhost:4445" % (username, access_key)
+driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+
+driver.get("http://www.python.org")
+print(driver.title)
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
