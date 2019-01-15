07d3098cf3fc72a075d0f26d143fe4d3412430ef
yanahini <36894930+yanahini@users.noreply.github.com>
2019-01-15 16:18:32 +0100

    Still not working

--- test_selenium.py
+++ test_selenium.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 
 username = os.environ["SAUCE_USERNAME"]
 access_key = os.environ["SAUCE_ACCESS_KEY"]
+
+capabilities = {}
+capabilities['browserName'] = 'Chrome'
 capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
 hub_url = "%s:%s@localhost:4445" % (username, access_key)
 driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
